bb720433e855aa8f3e3191508d8085a785c002cc
akhilranaaa <76087948+akhilranaaa@users.noreply.github.com>
2023-10-20 23:25:40 +0530

    Create FuelTypeRepository.java

---
+++ src/main/controller/src/main/java/org/ncu/hirewheels/repository/src/main/java/org/ncu/hirewheels/repository/src/main/java/org/ncu/hirewheels/repository/FuelTypeRepository.java
@@ -0,0 +1,7 @@
+package org.ncu.hirewheels.repository;
+
+import org.ncu.hirewheels.entities.FuelType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuelTypeRepository extends JpaRepository<FuelType, Long> {
+}
