b079fa54e44274f789f83042fa1c609444e9373f
akhilranaaa <76087948+akhilranaaa@users.noreply.github.com>
2023-10-21 15:41:59 +0530

    Delete src directory

--- src/main/controller/src/main/java/org/ncu/hirewheels/repository/src/main/java/org/ncu/hirewheels/repository/src/main/java/org/ncu/hirewheels/repository/FuelTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ncu.hirewheels.repository;
-
-import org.ncu.hirewheels.entities.FuelType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuelTypeRepository extends JpaRepository<FuelType, Long> {
-}
