fe5f7e52faf517be66f03259eb8d30c5586fc307
akhilranaaa <76087948+akhilranaaa@users.noreply.github.com>
2023-10-21 15:46:08 +0530

    Create HireWheelsApplicationTests.java

---
+++ src/test/java/org/ncu/hirewheels/HireWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+ackage org.ncu.hirewheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireWheelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
